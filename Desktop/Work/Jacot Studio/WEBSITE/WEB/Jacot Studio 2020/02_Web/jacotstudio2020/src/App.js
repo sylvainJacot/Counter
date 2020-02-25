dd2065a55a7f8000f4b6d61dcb60a469623f33e2
coatriver <sylvain.jacot@hotmail.be>
2020-02-25 15:41:17 +1100

    Setup mediaqueries

--- Desktop/Work/Jacot Studio/WEBSITE/WEB/Jacot Studio 2020/02_Web/jacotstudio2020/src/App.js
+++ Desktop/Work/Jacot Studio/WEBSITE/WEB/Jacot Studio 2020/02_Web/jacotstudio2020/src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import "./reset.css";
+import {media} from "./Atoms/mediaqueries";
+import styled from "styled-components";
+
+export const Square = styled.div`
+width: 400px;
+height: 400px;
+${media.mobile`
+
+background-color:red;
+`
+}
+${media.desktop`
+
+background-color:blue;
+`
+}
+
+`
 
 function App() {
   return (
     <div className="App">
      New Jacot Studio Website 2020 !
+        <Square></Square>
     </div>
   );
 }
