cb68cce18df2773c5e9cc5017a41e1fde51d354f
coatriver <sylvain.jacot@hotmail.be>
2020-02-25 15:12:17 +1100

    Setup: styled-components + sass + reset css + favicon

---
+++ Desktop/Work/Jacot Studio/WEBSITE/WEB/Jacot Studio 2020/02_Web/jacotstudio2020/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./reset.css";
+
+function App() {
+  return (
+    <div className="App">
+     New Jacot Studio Website 2020 !
+    </div>
+  );
+}
+
+export default App;
