b99d27afd1f8cc84b05019cdddf075da78498eea
coatriver <sylvain.jacot@hotmail.be>
2020-02-25 17:20:41 +1100

    Setup: fonts Playfair black qnd roboto regulard with Globalstyle from styled components

--- Desktop/Work/Jacot Studio/WEBSITE/WEB/Jacot Studio 2020/02_Web/jacotstudio2020/src/App.js
+++ Desktop/Work/Jacot Studio/WEBSITE/WEB/Jacot Studio 2020/02_Web/jacotstudio2020/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import "./reset.css";
 import {media} from "./Atoms/mediaqueries";
+import GlobalFonts from "./Atoms/fonts";
 import styled from "styled-components";
 
 export const Square = styled.div`
 width: 400px;
 height: 400px;
+
+& h1 {
+font-family: PlayfairDisplay-Regular_Black;
+font-size: 24px;
+
+}
+
+& p {
+font-family: Roboto;
+font-size: 16px;
+}
+
 ${media.mobile`
 
 background-color:red;
@@ -22,8 +35,9 @@ background-color:blue;
 function App() {
   return (
     <div className="App">
-     New Jacot Studio Website 2020 !
-        <Square></Square>
+        <GlobalFonts/>
+
+        <Square><h1>New Jacot Studio Website 2020 !</h1><p>Test de robot</p></Square>
     </div>
   );
 }
---
+++ Desktop/Work/Jacot Studio/WEBSITE/WEB/Jacot Studio 2020/02_Web/jacotstudio2020/src/Atoms/fonts.js
@@ -0,0 +1,25 @@
+/* seen on https://dev.to/alaskaa/how-to-import-a-web-font-into-your-react-app-with-styled-components-4-1dni */
+
+import {createGlobalStyle} from "styled-components";
+
+import PlayfairDisplayBlack from "../Atoms/fonts/Playfair_Display/PlayfairDisplay-Black.woff";
+import PlayfairDisplayBlack2 from "../Atoms/fonts/Playfair_Display/PlayfairDisplay-Black.woff2";
+
+import RobotoRegular from "../Atoms/fonts/Roboto/Roboto-Regular.woff";
+import RobotoRegular2 from "../Atoms/fonts/Roboto/Roboto-Regular.woff";
+
+export default createGlobalStyle`
+    @font-face {
+        font-family: 'Playfair Display';
+        url(${PlayfairDisplayBlack}) format('woff'),
+        url(${PlayfairDisplayBlack2}) format('woff2');
+    }
+    @font-face {
+    font-family: 'Roboto';
+    url(${RobotoRegular}) format('woff'),
+    url(${RobotoRegular2}) format('woff2'),
+    font-weight: normal;
+    font-style: normal;
+}
+
+`;
