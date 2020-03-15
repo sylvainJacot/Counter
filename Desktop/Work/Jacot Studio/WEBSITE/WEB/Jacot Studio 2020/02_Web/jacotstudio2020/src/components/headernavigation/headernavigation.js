d35dfffc27e0dab5596e4f59afff49c4e583e53b
coatriver <sylvain.jacot@hotmail.be>
2020-03-16 10:31:09 +1100

    Menu icon created - Hover effect done - Opened/closed to be done

--- Desktop/Work/Jacot Studio/WEBSITE/WEB/Jacot Studio 2020/02_Web/jacotstudio2020/src/components/headernavigation/headernavigation.js
+++ Desktop/Work/Jacot Studio/WEBSITE/WEB/Jacot Studio 2020/02_Web/jacotstudio2020/src/components/headernavigation/headernavigation.js
@@ -1,8 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-import {backgrounds, colorsRoles} from "../../Atoms/colors";
-import {LogoJacotStudio, IcLinkedin, IcInstagram, IcWhatsapp} from "../../Atoms/icons";
-
+import {colorsRoles} from "../Atoms/colors";
+import {LogoJacotStudio, IcBurgerMenu, IcLinkedin, IcInstagram, IcWhatsapp} from "../Atoms/icons";
+import {transitions} from "../Atoms/animations";
 
 
 export const HeaderWrapper = styled.header`
@@ -11,14 +11,40 @@ display: flex;
 align-items: center;
 justify-content: space-between;
 flex-direction: column;
-z-index: 1;
+z-index: 2;
 right: 0;
 top: 0;
 bottom: 0;
 width: 80px;
-background-color: indianred;
-/*background-color: $   {colorsRoles.White}10;*/
+background-color: ${colorsRoles.DarkGrey};
 padding: 32px 0;
+transition: ${transitions.basic2};
+
+&::after {
+display: block;
+position: absolute;
+z-index: -1;
+content: "";
+top: 0;
+right: 0;
+bottom: 0;
+left: 0;
+background-color: ${colorsRoles.White}10;
+}
+&:hover {
+width: 144px;
+transition: ${transitions.basic2};
+
+  & div > span {
+  height: 32px;
+  transition: ${transitions.basic1};
+  transition-delay: .1s;
+   &:nth-child(2) {
+   margin: 0 16px;
+   }
+  } 
+
+}
 
 `;
 export const SocialsWrapper = styled.ul`
@@ -40,8 +66,9 @@ export class HeaderNavigation extends React.Component {
             <>
                 <HeaderWrapper>
                  <LogoJacotStudio FillColor={colorsRoles.White} CircleColor={colorsRoles.LightGrey} Size={"40px"}/>
+                 <IcBurgerMenu/>
                 <SocialsWrapper>
-                    <a href="#"><IcLinkedin FillColor={colorsRoles.LightGrey} Size={"24px"}/></a>
+                    <a href="https://bashooka.com/coding/react-burger-menu-components/"><IcLinkedin FillColor={colorsRoles.LightGrey} Size={"24px"}/></a>
                     <a href="#"><IcInstagram FillColor={colorsRoles.LightGrey} Size={"24px"}/></a>
                     <a href="#"><IcWhatsapp FillColor={colorsRoles.LightGrey} Size={"24px"}/></a>
                 </SocialsWrapper>
