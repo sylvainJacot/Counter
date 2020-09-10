0a7ad087e41c715ea526a297ec1c28547da61108
coatriver <sylvain.jacot@hotmail.be>
2020-09-10 09:46:07 +0200

    Normalize, globalstyle created

---
+++ src/components/Counter.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+
+const Counter = () => {
+    return <>
+    Counter
+    </>
+
+};
+
+export default Counter;
---
+++ src/components/atoms/Colors.js
@@ -0,0 +1,5 @@
+export const Colors = {
+    White : "#FFFFFF",
+    DarkGrey : "#2F3640",
+    Black : "#000000",
+}
